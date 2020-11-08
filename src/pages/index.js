e6548145da7a957c85364c1654061e72430fd645
Aditya Agarwal <adityaa803@gmail.com>
2020-11-09 02:16:05 +0530

    checkpoint: after video

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,20 @@ function Home() {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p className="max-w-content text-3xl text-gray-700">Another new project? Don't tell me you already bought the domain. Hope this one gets completed Good Luck!</p>
+      <div className="bg-orange-400 min-h-screen flex justify-center items-center">
+        <div className="bg-white w-10/12">
+          <h1 className="text-3xl text-center font-bold mb-8 text-captain-500 mt-4">
+            Tailwind with Ankeet
+          </h1>
+          <div className="grid grid-rows-1 grid-flow-col gap-4 px-8 overflow-x-scroll pb-8">
+            {Array(14)
+              .fill(0)
+              .map((_, index) => (
+                <div key={index} className="w-16 h-16 md:w-32 md:h-32 rounded-full bg-gray-400"></div>
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
     extend: {
       maxWidth: {
         'content': '70ch',
-      }
+      },
+      spacing: {
+        7: '1.75rem',
+      },
+      textColor: {
+        captain: {
+          400: '#1229EA',
+          default: '#1849CA',
+          500: '#1849CA',
+          600: ''
+        }
+      },
     },
   },
   variants: {},
   plugins: [],
   purge: ['./src/**/*.js'],
-};
+}
